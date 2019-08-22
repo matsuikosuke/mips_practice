9bac1a68a839f074c0bef3d5d93025426e94e194
Kosuke.Matsui <kosukematsui@icloud.com>
2019-08-22 23:37:06 +0900

    状態遷移追加

--- maindec.v
+++ maindec.v
@@ -14,7 +14,7 @@ module maindec(
     output [1:0] aluop
     );
 
-reg [15:0] controls;
+reg [14:0] controls;
 reg [10:0] state, next_state;
 
 parameter S0 =  11'b000_0000_0000;
@@ -30,22 +30,72 @@ parameter S9 =  11'b001_0000_0000;
 parameter S10 = 11'b010_0000_0000;
 parameter S11 = 11'b100_0000_0000;
 
+
+assign {memtoreg, regdst, lord, pcsrc, alusrca, alusrcb,
+        irwrite, memwrite, pcwrite, branch, regwrite,
+        aluop} = controls;
+
 always @ (posedge clk, posedge reset)
     if (reset) state <= S0;
     else state <= next_state;
-
-assign {regwrite, 
-        regdst, alusrca, alusrcb, branch, pcwrite, pcsrc, memwrite, 
-        memtoreg, irwrite, lord, aluop} = controls;
+    
 
 always @ (*)
-    case(op)
-        6'b000000: controls <= 9'b1_1000_0010; //Rtype
-        6'b100011: controls <= 9'b1_0100_1000; //LW
-        6'b101011: controls <= 9'b0_0101_0000; //SW
-        6'b000100: controls <= 9'b0_0010_0001; //BEQ
-        6'b001000: controls <= 9'b1_0100_0000; //ADDI
-        6'b000010: controls <= 9'b0_0000_0100; //J
-        default: controls <= 9'bx_xxxx_xxxx;
+    case (state)
+        S0:
+            next_state = S1;
+            controls = 15'b00000001_10100_00;
+        S1:
+            if(op = 6'b100011) //LW
+                next_state = S2;
+            else(op = 6'b101011) //SW
+                next_state = S2;
+            else(op = 6'b000000) //Rtype
+                next_state = S6;
+            else(op = 6'b000100) //BEQ
+                next_state = S8;
+            else(op = 6'b001000) //ADDI
+                next_state = S9;
+            else(op = 6'b000010) //J
+                next_state = S11;
+            else
+                next_state = S1;
+            controls = 15'b00000011_00000_00;
+        S2:
+            if(op = 6'b100011) //LW
+                next_state = S3;
+            else(op = 6'b101011) //SW
+                next_state = S5;
+            else
+                next_state = S2;
+            controls = 15'b00000110_00000_00;
+        S3:
+            next_state = S4;
+            controls = 15'b00100000_00000_00;
+        S4:
+            next_state = S0;
+            controls = 15'b10000000_00001_00;
+        S5:
+            next_state = S0;
+            controls = 15'b00100000_01000_00;
+        S6:
+            next_state = S7;
+            controls = 15'b00000100_00000_10;
+        S7:
+            next_state = S0;
+            controls = 15'b01000000_00001_00;
+        S8:
+            next_state = S0;
+            controls = 15'b00001100_00010_01;
+        S9:
+            next_state = S10;
+            controls = 15'b00000110_00000_00;
+        S10:
+            next_state = S0;
+            controls = 15'b00000000_00001_00;
+        S11:
+            next_state = S0;
+            controls = 15'b00010000_00100_00;
+        default: controls = 15'bxxxxxxxx_xxxxx_xx;
     endcase
 endmodule
